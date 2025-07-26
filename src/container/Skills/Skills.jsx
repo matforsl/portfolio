84c6814948a9bc003de36eaa344690de0291d582
Matilda Forslund <matforsl@kth.se>
2025-07-26 22:50:50 +0200

    feat: add motion wrappers to components for animation

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { Tooltip } from 'react-tooltip';
 
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss'
 
@@ -87,4 +87,8 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'), 
+  "skills",
+  "app__whitebg"
+);
