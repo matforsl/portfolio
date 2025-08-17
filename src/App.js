e7997df8a11d034426c004afeba9ec1afc545617
Matilda Forslund <matforsl@kth.se>
2025-08-17 15:29:06 +0200

    chore: code clean up

--- src/App.js
+++ src/App.js
@@ -14,26 +14,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-
-// shortcut to generate base: rafce
-/*
-import React from 'react';
-import { Footer, Header, Skills, Work } from './container';
-import { Navbar } from './components';
-import './App.scss';
- 
-const App = () => {
-  return (
-    <div className='app'>
-        <Navbar />
-        <Header />
-        <Work />
-        <Skills />
-        <Footer />
-    </div>
-  )
-}
-
-export default App;
-*/
