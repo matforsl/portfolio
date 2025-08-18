dac3f971937d186ef1af7909f637df57ab02bfef
Matilda Forslund <matforsl@kth.se>
2025-08-18 10:28:59 +0200

    fix: Remove form and address security issues

--- src/client.js
+++ src/client.js
@@ -1,12 +1,11 @@
-import sanityClient from '@sanity/client';
+import {createClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
+export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2022-02-01',
+  apiVersion: '2025-08-18',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
